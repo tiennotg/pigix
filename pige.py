857209545db18f1796ce89548075b7d641972b18
Guilhem Tiennot <guilhem@tiennotblog.fr>
2014-05-08 14:25:11 +0200

    Better use of argparse.

--- pige.py
+++ pige.py
@@ -9,13 +9,11 @@ from os.path import expanduser,join
 parser = argparse.ArgumentParser(description="Radio recorder.")
 parser.add_argument('duration',type=int,help='Duration of the recorded file.')
 parser.add_argument('directory',type=str,help='Directory where the recorded files are stored.')
-parser.add_argument('--rec-dir',help="Directory where the cue file is recorded.")
-parser.add_argument('--log-dir',help="Directory where logs are stored. Default: $HOME.")
+parser.add_argument('--rec-dir',type=str,help="Directory where the cue file is recorded.")
+parser.add_argument('--log-dir',type=str,default="~",help="Directory where logs are stored. Default: $HOME.")
 
 args = parser.parse_args()
 
-if not args.log_dir:
-	args.log_dir = '~'
 logging.basicConfig(filename=join(expanduser(args.log_dir),'logs'),
 					format='[%(asctime)s] %(levelname)s: %(message)s',
 					level=logging.INFO)
