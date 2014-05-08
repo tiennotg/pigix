2754ff0adaffdc1796fb712cdcf31f95875a7134
Guilhem Tiennot <guilhem@tiennotblog.fr>
2014-05-08 15:16:34 +0200

    Make code more beautiful :-)

--- rotate.py
+++ rotate.py
@@ -2,45 +2,46 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import os
+from os.path import expanduser,join,isfile,abspath, getsize
+from os import remove,rename,listdir
 import re
 import datetime
 
 def __rotate_records(directory, rotation):
-	directory = os.path.abspath(os.path.expanduser(directory))
-	files = os.listdir(directory)
+	directory = abspath(expanduser(directory))
+	files = listdir(directory)
 	limit = datetime.date.today() - datetime.timedelta(rotation)
 	p = re.compile("([0-9]{8})(_[0-9]{4}){2}\..{2,4}")
 	
 	for f in files:
 		m = p.match(f)
-		if os.path.isfile(os.path.join(directory,f)) and m:
+		if isfile(join(directory,f)) and m:
 			date = m.group(1)
 			date = datetime.date(int(date[0:4]),int(date[4:6]),int(date[6:8]))
 
 			if date < limit:
-				os.remove(os.path.join(directory,f))
+				remove(join(directory,f))
 	
 def __rotate_logs(size):
-	log = os.path.expanduser("~/logs")
-	home = os.path.abspath(os.path.expanduser("~"))
+	log = expanduser("~/logs")
+	home = abspath(expanduser("~"))
 	
-	if os.path.isfile(log) and os.path.getsize(log) >= size:
-		files = os.listdir(home)
+	if isfile(log) and getsize(log) >= size:
+		files = listdir(home)
 		p = re.compile("logs\.([0-9]*)?")
 		n = None;
 		
 		for f in files:
 			m = p.match(f)
-			if os.path.isfile(os.path.join(home,f)) and m:
+			if isfile(join(home,f)) and m:
 				if n == None or int(m.group(1)) > n:
 					n = int(m.group(1))
 		
 		if n != None:
 			for i in reversed(range(0,n+1)):
-				if os.path.isfile(os.path.join(home,"logs."+str(i))):
-					os.rename(os.path.join(home,"logs."+str(i)),os.path.join(home,"logs."+str(i+1)))
-		os.rename(log,os.path.join(home,"logs.0"))
+				if isfile(join(home,"logs."+str(i))):
+					rename(join(home,"logs."+str(i)),join(home,"logs."+str(i+1)))
+		rename(log,join(home,"logs.0"))
 		
 
 parser = argparse.ArgumentParser(description="Logs and records rotation.")
