377eddcec6e2e28e179e780c256a501427eb28c4
Guilhem Tiennot <guilhem@tiennotblog.fr>
2014-05-10 00:11:25 +0200

    Make the code cleaner.

--- rotate.py
+++ rotate.py
@@ -8,9 +8,11 @@ import re
 import datetime
 from const import LOG_FILE
 
-def __rotate_records(directory, rotation):
-	directory = abspath(expanduser(directory))
-	files = listdir(directory)
+def __list_files(directory):
+	return listdir(abspath(expanduser(directory)))
+
+def rotate_records(directory, rotation):
+	files = __list_files(directory)
 	limit = datetime.date.today() - datetime.timedelta(rotation)
 	p = re.compile("([0-9]{8})(_[0-9]{4}){2}\..{2,4}")
 	
@@ -23,36 +25,37 @@ def __rotate_records(directory, rotation):
 			if date < limit:
 				remove(join(directory,f))
 	
-def __rotate_logs(size):
-	log = expanduser("~/logs")
-	home = abspath(expanduser("~"))
+def rotate_logs(directory, size):
+	directory = abspath(expanduser(directory))
+	log = expanduser(join(directory,LOG_FILE))
 	
 	if isfile(log) and getsize(log) >= size:
-		files = listdir(home)
-		p = re.compile("logs\.([0-9]*)?")
+		files = __list_files(directory)
+		p = re.compile(LOG_FILE+"\.([0-9]*)?")
 		n = None;
 		
 		for f in files:
 			m = p.match(f)
-			if isfile(join(home,f)) and m:
+			if isfile(join(directory,f)) and m:
 				if n == None or int(m.group(1)) > n:
 					n = int(m.group(1))
 		
 		if n != None:
 			for i in reversed(range(0,n+1)):
-				if isfile(join(home,"logs."+str(i))):
-					rename(join(home,"logs."+str(i)),join(home,"logs."+str(i+1)))
-		rename(log,join(home,"logs.0"))
+				if isfile(join(directory,LOG_FILE+"."+str(i))):
+					rename(join(directory,LOG_FILE+"."+str(i)),join(directory,LOG_FILE+"."+str(i+1)))
+		rename(log,join(directory,LOG_FILE+".0"))
 		
 
 parser = argparse.ArgumentParser(description="Logs and records rotation.")
 parser.add_argument('directory',type=str,help='Directory where the recorded files are stored.')
 parser.add_argument('rotation',type=int,help='Rotation period (in days).')
 parser.add_argument('size',type=int,help='Maximal size for log files (in bytes).')
+parser.add_argument('--log-dir',type=str,default="~",help="Directory where logs are stored. Default: $HOME.")
 
 args = parser.parse_args()
 
-__rotate_records(args.directory,args.rotation)
-__rotate_logs(args.size)
+rotate_records(args.directory,args.rotation)
+rotate_logs(args.log_dir,args.size)
 
 exit(0)
